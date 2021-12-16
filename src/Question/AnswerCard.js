46686e34060c5154ce5cfd7684d1762e690dda59
antonin <antonin.val@gmail.com>
2021-12-16 10:51:53 +0100

    ok route

--- src/Question/AnswerCard.js
+++ src/Question/AnswerCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {useNavigate} from "react-router";
 
-const AnswerCard = () => {
+const AnswerCard = ({text, isValid, idQuestion}) => {
+    console.log(isValid);
+    const history = useNavigate();
+    console.log(typeof parseInt(idQuestion))
 
-    let navigate = useNavigate();
 
-    function redirectPath() {
-        navigate("question/1", {replace: true});
+    function changeHistory (){
+        history(`/question/${parseInt(idQuestion)+1}`);
     }
 
-
     return (
-        <div className="w-32 border-white rounded-3xl bg-white" onClick={redirectPath}>
-            Réponse x
+        <div className="w-32 border-white rounded-3xl bg-white" onClick={() => changeHistory()}>
+            {text}
         </div>
     );
 };
--- src/Question/Question.js
+++ src/Question/Question.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router";
 import AnswerCard from "./AnswerCard";
 
 const Question = ({name}) => {
     const {id} = useParams()
+    const port = process.env.PORT || "http://localhost:3000"
+    const [question, setQuestion] = useState(null);
+
+    useEffect(() => {
+        async function fetchQuestion () {
+            const question = await fetch(`${port}/question`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({id: id})
+            }
+            );
+            setQuestion(await question.json());
+
+        }
+    fetchQuestion();
+    }, [id])
     return (
         <div className="bg-black w-screen h-screen">
             <p className="text-left text-white w-screen ml-3 pt-3">{name}</p>
-            <div className="flex flex-col pt-48">
-                <h2 className="text-white mb-10">exemple question .......</h2>
+            {question && (<div className="flex flex-col pt-48">
+                <h2 className="text-white mb-10">{question.question}</h2>
                 <div className="flex flex-col mx-auto ">
                     <div className="flex mb-4">
-                        <AnswerCard/>
-                        <AnswerCard/>
+                        <AnswerCard text={question.answer[0].text} isValid={question.answer[0].isValid} idQuestion={id}/>
+                        <AnswerCard text={question.answer[1].text} isValid={question.answer[1].isValid} idQuestion={id}/>
                     </div>
                     <div className="flex">
-                        <AnswerCard/>
-                        <AnswerCard/>
+                        <AnswerCard text={question.answer[2].text} isValid={question.answer[2].isValid} idQuestion={id}/>
+                        <AnswerCard text={question.answer[3].text} isValid={question.answer[3].isValid} idQuestion={id}/>
                     </div>
                 </div>
 
-            </div>
+            </div>)}
+
         </div>
     );
 };
